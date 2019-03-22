862f5ede3868a28acc4026b621ec3a4e0c59fe74
Felix <ff@rapidfacture.com>
2019-03-22 17:06:52 +0100

    update examples

--- examples/server.js
+++ examples/server.js
@@ -4,10 +4,10 @@ var config = require('./config/config.js');
 
 // init
 var mailer = simpleTemplateMailer({ // create a instance
-  transporter: config.transporter, // mail options from external configuration file
-  translationsPath: "./translations",
-  templatesPath: "./templates",
-  defaultLanguage: "de"
+   transporter: config.transporter, // mail options from external configuration file
+   translationsPath: './translations',
+   templatesPath: './templates',
+   defaultLanguage: 'de'
 });
 
 
@@ -15,40 +15,41 @@ var mailer = simpleTemplateMailer({ // create a instance
 // call with several options
 mailer.getTemplate({ // template options
 
-    name: 'newsletter', // template folder name
+   name: 'newsletter', // template folder name
 
-    language: "de", // select json translation file
+   language: 'de', // select json translation file
 
-    data: { // data from your app for mustache
-      testData: "HelloWorld"
-    }
-  },
-  function(template) { // on success
-      console.log(template);
-  }
+   data: { // data from your app for mustache
+      testData: 'HelloWorld'
+   }
+},
+function (template) { // on success
+   console.log(template);
+}
 );
 
 
 // call with several options
 mailer.mail({ // template options
 
-    name: 'newsletter', // template folder name
+   name: 'newsletter', // template folder name
 
-    language: "de", // select json translation file
+   language: 'de', // select json translation file
 
-    data: { // data from your app for mustache
-      testData: "HelloWorld"
-    }
-  }, { // nodemailer options; recipients need to be specified
+   data: { // data from your app for mustache
+      testData: 'HelloWorld'
+   }
+}, { // nodemailer options; recipients need to be specified
 
-    to: ["max.mustermann@web.net"]
-    /* further parameter: https://nodemailer.com/message/
+   to: ['max.mustermann@web.net']
+   /* further parameter: https://nodemailer.com/message/
     attachements: files,
     replyTo: ["max.muster@web.net"]
     */
-  },
-
-  function(data, info) {}, // on sucess
-
-  function(err, info) {} // on error
+}, function (data, info) {
+   console.log('success', data, info);
+}, // on sucess
+function (err, info) {
+   console.log('err', err, info);
+} // on error
 );
